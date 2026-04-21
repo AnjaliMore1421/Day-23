6b0ee6290d7922e7aabdbcd003531e08c6eca20d
AnjaliMore1421 <moreanjali219@gmail.com>
2026-04-21 11:49:34 +0530

    Create theme.js

---
+++ styled-ui-app/styles/theme.js
@@ -0,0 +1,15 @@
+export const lightTheme = {
+  bg: "#f1f5f9",
+  text: "#0f172a",
+  card: "#ffffff",
+  primary: "#2563eb",
+  sidebar: "#1e293b"
+};
+
+export const darkTheme = {
+  bg: "#020617",
+  text: "#e2e8f0",
+  card: "#0f172a",
+  primary: "#3b82f6",
+  sidebar: "#020617"
+};
